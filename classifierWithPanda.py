aa653b22ad41bf42c333055088a1d7ae8242b79e
Greg Garnhart <gegarnhart@email.wm.edu>
2019-03-21 19:24:49 -0400

    GOT THE DATA READ in correctly lol

--- classifierWithPanda.py
+++ classifierWithPanda.py
@@ -1,2 +1,39 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+from IPython.display import display  # try to fix that display issue
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+# Local utilities. We're gonna use this to split the data.
+from csci416 import read_mnist, compute_metrics
+from sklearn.model_selection import train_test_split
+
 data = pd.read_csv('data.csv')
+print("let's check the data")
+display(data.head())
+display(data.describe())  # good up to here!
+
+f = open("data.csv")
+data_np = np.loadtxt(f, delimiter=',')  # LETS GOOOO
+m, n = data_np.shape
+X = data_np[:, 0:6]
+Y = data_np[:, 7]
+
+# # Let's use the nice csci416 function to split the data pls.
+# test_size_amount = .7
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=test_size_amount, random_state=42)
--- glassTutorialClassification.py
+++ glassTutorialClassification.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np  # Graphs and stuff
-# Seaborn is a library based on matplotlib and has nice functionalities for drawing graphs.
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import time
-
+from IPython.display import display  # try to fix that display issue
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
@@ -24,17 +23,17 @@ filename_glass = './glass.csv'
 df_glass = pd.read_csv(filename_glass)
 
 print(df_glass.shape)
+display(df_glass.head())
+display(df_glass.describe())
 
 
-def get_training_set(df, y_col, x_cols, ratio):
+def get_train_test(df, y_col, x_cols, ratio):
     """ 
-    This method transforms a dataframe (df) into a train and test set, for this you need to specify:
+    This method transforms a dataframe into a train and test set, for this you need to specify:
     1. the ratio train : test (usually 0.7)
     2. the column with the Y_values
     """
-
-    mask = np.random.rand(len(df))
-    # come back here, perhaps. idk if this is good yet
+    mask = np.random.rand(len(df)) < ratio
     df_train = df[mask]
     df_test = df[~mask]
 
@@ -47,21 +46,8 @@ def get_training_set(df, y_col, x_cols, ratio):
 
 y_col_glass = 'Type'
 x_cols_glass = list(df_glass.columns.values)
-x_cols_glass.remove(y_col_glass)
+# x_cols_glass.remove(y_col_glass)
 
 train_test_ratio = 0.7
-df_train, df_test, X_train, Y_train, X_test, Y_test = get_training_set(
+df_train, df_test, X_train, Y_train, X_test, Y_test = get_train_test(
     df_glass, y_col_glass, x_cols_glass, train_test_ratio)
-
-# Try all of the classifier types!
-
-dict_classifiers = {
-    "Logistic Regression": LogisticRegression(),
-    "Nearest Neighbors": KNeighborsClassifier(),
-    "Linear SVM": SVC(),
-    "Gradient Boosting Classifier": GradientBoostingClassifier(n_estimators=1000),
-    "Decision Tree": tree.DecisionTreeClassifier(),
-    "Random Forest": RandomForestClassifier(n_estimators=1000),
-    "Neural Net": MLPClassifier(alpha=1),
-    "Naive Bayes": GaussianNB()
-}
